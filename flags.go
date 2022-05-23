5f82f464af4eef4fcd14b80576b7f021655df1f2
Victor Ndaba <mwangindaba@gmail.com>
2022-05-23 20:19:44 +0300

    📦 feat: created the `new_flag` method

--- flags.go
+++ flags.go
@@ -1,8 +1,32 @@
 package gommander
 
+import "strings"
+
 type Flag struct {
 	name  string
 	long  string
 	short string
 	help  string
+}
+
+func new_flag(val string, help string) Flag {
+	values := strings.Split(val, " ")
+
+	short := ""
+	long := ""
+
+	for _, v := range values {
+		if strings.HasPrefix(v, "--") {
+			long = v
+		} else if strings.HasPrefix(v, "-") {
+			short = v
+		}
+	}
+
+	return Flag{
+		name:  strings.Replace(long, "-", "", -1),
+		long:  long,
+		short: short,
+		help:  help,
+	}
 }
