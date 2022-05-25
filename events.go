0f465c23e50b3c2745b9e5de1e6f45d0340f4f2d
Victor Ndaba <mwangindaba@gmail.com>
2022-05-25 19:52:52 +0300

    📦 feat: completed base functionality of events emitter

--- events.go
+++ events.go
@@ -1,16 +1,80 @@
 package gommander
 
-type EventListener = func(EventConfig)
+import (
+	"sort"
+)
+
+type Event byte
+type EventCallback = func(*EventConfig)
+
+const (
+	MissingRequiredArgument Event = iota
+	OptionMissingArgument
+	OutputHelp
+	OutputVersion
+	UnknownCommand
+	UnknownOption
+	UnresolvedArgument
+)
+
+type EventListener struct {
+	cb    EventCallback
+	index int
+}
 
 type EventConfig struct {
+	args      []string
+	event     Event
+	app_ref   *Command
+	exit_code int
+	err       string
 }
 
 type EventEmitter struct {
-	listeners []EventListener
+	listeners map[Event][]EventListener
 }
 
-func NewEmitter() *EventEmitter {
-	return &EventEmitter{
-		listeners: []EventListener{},
+func (c *EventConfig) GetArgs() *[]string { return &c.args }
+func (c *EventConfig) GetEvent() Event    { return c.event }
+func (c *EventConfig) GetApp() *Command   { return c.app_ref }
+func (c *EventConfig) GetExitCode() int   { return c.exit_code }
+func (c *EventConfig) GetError() string   { return c.err }
+
+func new_emitter() EventEmitter {
+	return EventEmitter{
+		listeners: make(map[Event][]EventListener),
+	}
+}
+
+func (em *EventEmitter) on(event Event, cb EventCallback, pstn int) {
+	if len(em.listeners) == 0 {
+		em.listeners[event] = []EventListener{{cb, pstn}}
+	} else {
+		new_v := em.listeners[event]
+
+		for e := range em.listeners {
+			if e == event {
+				new_v = append(new_v, EventListener{cb, pstn})
+			}
+		}
+
+		em.listeners[event] = new_v
+	}
+
+}
+
+func (em *EventEmitter) emit(cfg EventConfig) {
+	event := cfg.GetEvent()
+
+	for e, v := range em.listeners {
+		if e == event {
+			sort.SliceStable(v, func(i, j int) bool {
+				return v[j].index > v[i].index
+			})
+
+			for _, lstnr := range v {
+				lstnr.cb(&cfg)
+			}
+		}
 	}
 }
