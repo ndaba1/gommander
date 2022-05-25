6548c082bc8e40e6d3d1e3b11a5df448ecf98c8c
Victor Ndaba <mwangindaba@gmail.com>
2022-05-25 20:03:29 +0300

    ✨ style: ordered methods alphabetically and semantically

--- gommander.go
+++ gommander.go
@@ -4,37 +4,38 @@ import (
 	"fmt"
 )
 
-type Callback = func(ParserMatches)
+type CommandCallback = func(*ParserMatches)
 
-type App struct {
-	name         string
+type Command struct {
 	alias        string
 	arguments    []*Argument
+	callback     CommandCallback
+	discussion   string
+	emitter      EventEmitter
 	flags        []*Flag
-	options      []*Option
-	sub_commands []*App
-	parent       *App
 	help         string
-	version      string
+	is_root      bool
+	name         string
+	options      []*Option
+	parent       *Command
+	sub_commands []*Command
 	settings     Settings
 	theme        Theme
-	discussion   string
-	is_root      bool
-	callback     Callback
+	version      string
 }
 
-func Program() *App {
-	return Command("").set_is_root(true)
+func Program() *Command {
+	return App("").set_is_root(true)
 }
 
-func Command(name string) *App {
-	return &App{
+func App(name string) *Command {
+	return &Command{
 		name:         name,
 		alias:        "",
 		arguments:    []*Argument{},
 		flags:        []*Flag{},
 		options:      []*Option{},
-		sub_commands: []*App{},
+		sub_commands: []*Command{},
 		parent:       nil,
 		help:         "",
 		version:      "",
@@ -42,88 +43,112 @@ func Command(name string) *App {
 		theme:        Theme{},
 		discussion:   "",
 		is_root:      false,
+		emitter:      new_emitter(),
 	}
 }
 
 // Value getters
-func (app *App) GetName() string { return app.name }
-
-func (app *App) GetAlias() string { return app.alias }
-
-func (app *App) GetArguments() []*Argument { return app.arguments }
-
-func (app *App) GetFlags() []*Flag { return app.flags }
-
-func (app *App) GetOptions() []*Option { return app.options }
-
-func (app *App) GetSubCommands() []*App { return app.sub_commands }
-
-func (app *App) GetParent() *App { return app.parent }
-
-func (app *App) GetHelp() string { return app.help }
-
-func (app *App) GetVersion() string { return app.version }
+func (c *Command) GetAlias() string           { return c.alias }
+func (c *Command) GetArguments() []*Argument  { return c.arguments }
+func (c *Command) GetFlags() []*Flag          { return c.flags }
+func (c *Command) GetHelp() string            { return c.help }
+func (c *Command) GetName() string            { return c.name }
+func (c *Command) GetOptions() []*Option      { return c.options }
+func (c *Command) GetParent() *Command        { return c.parent }
+func (c *Command) GetSubCommands() []*Command { return c.sub_commands }
+func (c *Command) GetVersion() string         { return c.version }
 
 // Value setters
-func (app *App) Name(name string) *App {
-	app.name = name
-	return app
+
+func (c *Command) Action(cb CommandCallback) *Command {
+	c.callback = cb
+	return c
 }
 
-func (app *App) Alias(alias string) *App {
-	app.alias = alias
-	return app
+func (c *Command) Alias(alias string) *Command {
+	c.alias = alias
+	return c
 }
 
-func (app *App) Help(help string) *App {
-	app.help = help
-	return app
-}
-
-func (app *App) Version(version string) *App {
-	app.version = version
-	return app
-}
-
-func (app *App) Action(cb Callback) *App {
-	app.callback = cb
-	return app
-}
-
-func (app *App) Argument(val string, help string) *App {
+func (c *Command) Argument(val string, help string) *Command {
 	argument := new_argument(val, help)
-	app.arguments = append(app.arguments, argument)
-	return app
+	c.arguments = append(c.arguments, argument)
+	return c
 }
 
-func (app *App) Flag(val string, help string) *App {
+func (c *Command) AddFlag(flag *Flag) *Command {
+	c.flags = append(c.flags, flag)
+	return c
+}
+
+func (c *Command) AddOption(opt *Option) *Command {
+	c.options = append(c.options, opt)
+	return c
+}
+
+func (c *Command) Flag(val string, help string) *Command {
 	flag := new_flag(val, help)
-	app.flags = append(app.flags, &flag)
-	return app
+	return c.AddFlag(&flag)
 }
 
-func (app *App) Option(val string, help string) *App {
+func (c *Command) Help(help string) *Command {
+	c.help = help
+	return c
+}
+
+func (c *Command) Name(name string) *Command {
+	c.name = name
+	return c
+}
+
+func (c *Command) Version(version string) *Command {
+	c.version = version
+	return c
+}
+
+func (c *Command) Option(val string, help string) *Command {
 	option := new_option(val, help, false)
-	app.options = append(app.options, &option)
-	return app
+	return c.AddOption(&option)
 }
 
-func (app *App) Subcommand(name string) *App {
-	return Command(name).set_parent(app)
+func (c *Command) RequiredOption(val string, help string) *Command {
+	opt := new_option(val, help, true)
+	return c.AddOption(&opt)
+}
+
+func (c *Command) Subcommand(name string) *Command {
+	return App(name).set_parent(c)
+}
+
+func (c *Command) On(event Event, cb EventCallback) {
+	c.emitter.on(event, cb, 0)
+}
+
+func (c *Command) BeforeAll(cb EventCallback) {
+	c.emitter.on(OutputHelp, cb, -5)
+}
+
+func (c *Command) Emit(cfg EventConfig) {
+	c.emitter.emit(cfg)
+}
+
+func (c *Command) TestEmit() {
+	c.Emit(EventConfig{[]string{""}, OutputHelp, c, int(1), c.alias})
+
 }
 
 // Interior utility functions
-func (app *App) set_parent(parent *App) *App {
+func (app *Command) set_parent(parent *Command) *Command {
 	app.parent = parent
 	return app
 }
 
-func (app *App) set_is_root(val bool) *App {
+func (app *Command) set_is_root(val bool) *Command {
 	app.is_root = val
 	return app
 }
 
-func (app *App) PrintHelp() {
+func (app *Command) PrintHelp() {
 	fmt.Printf(app.help)
 
 	fmt.Printf("\n USAGE: \n")
