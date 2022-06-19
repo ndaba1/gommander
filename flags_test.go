6796de526ec4ecc460f83998ea42752f22f6e0e8
Victor Ndaba <mwangindaba@gmail.com>
2022-06-19 21:39:20 +0300

    🔧 fix: changed vars naming convention

--- flags_test.go
+++ flags_test.go
@@ -4,24 +4,24 @@ import "testing"
 
 func TestFlagsCreation(t *testing.T) {
 	flag := NewFlag("help").Short('h').Help("The help flag")
-	flag_2 := new_flag("-h --help", "The help flag")
+	flagB := newFlag("-h --help", "The help flag")
 
-	if !flag.compare(&flag_2) {
+	if !flag.compare(&flagB) {
 		t.Errorf("Flag creation functions out of sync: 1. %v  2. %v",
-			flag, flag_2,
+			flag, flagB,
 		)
 	}
 
 	flag.Global(true)
-	if !flag.is_global {
+	if !flag.isGlobal {
 		t.Error("Failed to set flag as global")
 	}
 
-	exp_l := "-h, --help"
-	exp_f := "The help flag"
+	expL := "-h, --help"
+	expF := "The help flag"
 
-	if l, f := flag.generate(); l != exp_l || f != exp_f {
-		t.Errorf("Flag generate functioning incorrectly. Expected (%v, %v), but found (%v, %v)", exp_l, exp_f, l, f)
+	if l, f := flag.generate(); l != expL || f != expF {
+		t.Errorf("Flag generate functioning incorrectly. Expected (%v, %v), but found (%v, %v)", expL, expF, l, f)
 	}
 }
 
@@ -33,6 +33,6 @@ func BenchmarkFlagsBuilder(b *testing.B) {
 
 func BenchmarkNewFlagFn(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		new_flag("-V --version", "A version flag")
+		newFlag("-V --version", "A version flag")
 	}
 }
