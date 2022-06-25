e855d666625cdd00eaad92c2ab93c1706f9bb1ca
Victor Ndaba <mwangindaba@gmail.com>
2022-06-26 00:37:48 +0300

    🧪 test: updated tests for package interface

--- gommander_test.go
+++ gommander_test.go
@@ -1,6 +1,8 @@
 package gommander
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCommandMetadata(t *testing.T) {
 	cmd := NewCommand("basic").Help("Basic command")
@@ -29,6 +31,34 @@ func TestCommandSettings(t *testing.T) {
 		t.Error("Failed to disable version flag")
 	}
 
+}
+
+func TestEventListeners(t *testing.T) {
+	app := App()
+
+	app.SubCommand("test")
+
+	app.Override(UnknownCommand, func(ec *EventConfig) {
+		if len(ec.GetArgs()) != 1 {
+			t.Error("Incorrect number of args passed along")
+		}
+		if ec.GetEvent() != UnknownCommand {
+			t.Error("Event on EventCfg set incorrectly")
+		}
+		if ec.GetApp() != app {
+			t.Error("app reference passed incorrectly")
+		}
+		if ec.GetError().message != "no such subcommand found: `new`" {
+			t.Error("Error message configured wrongly")
+		}
+		if ec.GetExitCode() != 40 {
+			t.Error("Wrong exit code found")
+		}
+	})
+
+	app.Set(IncludeHelpSubcommand, true)
+
+	app.ParseFrom([]string{"my_bin", "new"})
 }
 
 func BenchmarkBuildEmpty(b *testing.B) {
