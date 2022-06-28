49a6a68db3a170b297eb78f1a98fded0823add11
Victor Ndaba <mwangindaba@gmail.com>
2022-06-28 23:12:59 +0300

    🧪 test: updated tests with assertion utils, much cleaner

--- args_test.go
+++ args_test.go
@@ -2,13 +2,11 @@ package gommander
 
 import (
 	"fmt"
-	"io"
-	"os"
 	"strconv"
 	"testing"
 )
 
-func TestXxx(t *testing.T) {
+func Test_(t *testing.T) {
 	// dummy function to set test_mode env vars
 	// Workaround to set test mode once for all other tests
 	setGommanderTestMode()
@@ -18,23 +16,12 @@ func TestArgsCreation(t *testing.T) {
 	arg := NewArgument("<test>").Help("Test argument").Variadic(true)
 	argB := newArgument("<test...>", "Test argument")
 
-	if !arg.compare(argB) {
-		t.Errorf("Arg creation methods out of sync: 1: %v 2: %v", arg, argB)
-	}
-
-	if !arg.isRequired {
-		t.Errorf("Failed to make arg required")
-	}
-	if !arg.isVariadic {
-		t.Errorf("Failed to make arg variadic")
-	}
-	if arg.name != "test" {
-		t.Errorf("Arg name not set correctly")
-	}
-	if arg.help != "Test argument" {
-		t.Errorf("Arg help string wrongly set")
-	}
-
+	assertStructEq[*Argument](t, arg, argB, "Arg creation methods out of sync")
+	assert(t, arg.compare(argB)) // workaround to suppress linter warnings
+	assert(t, arg.isRequired, "Failed to make arg required")
+	assert(t, arg.isVariadic, "Failed to make arg variadic")
+	assertEq(t, arg.name, "test", "Arg name not set correctly")
+	assertEq(t, arg.help, "Test argument", "Arg help string wrongly set")
 }
 
 func TestArgsMetadata(t *testing.T) {
@@ -43,37 +30,32 @@ func TestArgsMetadata(t *testing.T) {
 		Help("Test argument").
 		ValidateWith([]string{"ONE", "TWO"})
 
-	if !arg.testValue("one") && arg.testValue("TWO") {
-		t.Errorf("Arg validation working incorrectly")
-	}
-	if arg.getRawValue() != "<basic...>" {
-		t.Errorf("Raw value return function working incorrectly")
-	}
+	assertNe(t, arg.name, "<basic>", "Enclosures not stripped from name")
+	assert(t, arg.testValue("one"), "Arg validation working incorrectly")
+	assert(t, arg.testValue("TWO"), "Arg validation working incorrectly")
+	assertEq(t, arg.getRawValue(), "<basic...>", "Raw value return function working incorrectly")
 
 	expLeading := "<basic...>"
 	expFloating := "Test argument"
+	gotLeading, gotFloating := arg.generate()
 
-	if l, f := arg.generate(); l != expLeading || f != expFloating {
-		t.Errorf("The arg generate function is problematic. Expected: (%v, %v) but found (%v, %v)", expLeading, expFloating, l, f)
-	}
+	assertEq(t, expLeading, gotLeading, "The arg generate function is problematic")
+	assertEq(t, expFloating, gotFloating, "The arg generate function is problematic")
 }
 
 func TestOptionalArgs(t *testing.T) {
 	arg := NewArgument("[optional]").Default("DEFAULT").Help("Optional value with default")
 
-	if arg.isRequired {
-		t.Error("Failed to set argument as optional")
-	}
-	if !arg.hasDefaultValue() || arg.defaultValue != "DEFAULT" {
-		t.Error("Failed to set default value for argument")
-	}
+	assert(t, !arg.isRequired, "Failed to set argument as optional")
+	assert(t, arg.hasDefaultValue(), "Failed to set default value")
+	assert(t, arg.defaultValue == "DEFAULT", "Failed to set default value")
 
 	expLeading := "[optional]"
 	expFloating := "Optional value with default (default: DEFAULT)"
+	gotLeading, gotFloating := arg.generate()
 
-	if l, f := arg.generate(); l != expLeading || f != expFloating {
-		t.Errorf("The arg generate function is problematic. Expected: (%v, %v) but found (%v, %v)", expLeading, expFloating, l, f)
-	}
+	assertEq(t, expLeading, gotLeading, "The arg generate function is problematic")
+	assertEq(t, expFloating, gotFloating, "The arg generate function is problematic")
 }
 
 func TestArgValidValues(t *testing.T) {
@@ -82,21 +64,14 @@ func TestArgValidValues(t *testing.T) {
 		DisplayAs("language").
 		ValidateWith([]string{"ENG", "SPA", "RUS", "FRE"})
 
-	if arg.getRawValue() != "language" {
-		t.Error("Failed to set raw arg value using DisplayAs method")
-	}
-	if arg.testValue("else") {
-		t.Error("Values validation not working properly")
-	}
+	assert(t, !arg.testValue("else"), "Values validation not working properly")
+	assertEq(t, arg.getRawValue(), "language", "Failed to set raw arg value using DisplayAs method")
 
 	exec := func() {
 		arg.Default("NEW")
 	}
 	expected := fmt.Sprintf("error occurred when setting default value for argument: %v \n.  the passed value %v does not match the valid values: %v", arg.name, "NEW", arg.validValues)
-
-	if !assertStdOut(expected, exec) {
-		t.Error("Argument validation for arguments with valid values is buggy")
-	}
+	assertStdOut(t, expected, exec, "Argument validation for arguments with valid values is buggy")
 }
 
 func TestArgValidatorFunc(t *testing.T) {
@@ -110,36 +85,14 @@ func TestArgValidatorFunc(t *testing.T) {
 			return nil
 		})
 
-	if arg.getRawValue() != "int" {
-		t.Error("Failed to set raw arg value using DisplayAs method")
-	}
-	if !arg.testValue("2") {
-		t.Error("Strconv validator function working incorrectly")
-	}
+	assertEq(t, arg.getRawValue(), "int", "Failed to set raw arg value using DisplayAs method")
+	assert(t, arg.testValue("2"), "Strconv validator function working incorrectly")
 
 	exec := func() {
 		arg.Default("notInt")
 	}
 	expected := fmt.Sprintf("you tried to set a default value for argument: %v, but the validator function returned an error for values: %v", arg.name, "notInt")
-
-	if !assertStdOut(expected, exec) {
-		t.Error("Argument validation for arguments with validator functions is buggy")
-	}
-}
-
-func assertStdOut(expected string, exec func()) bool {
-	stdOut := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	exec()
-	_ = w.Close()
-	res, _ := io.ReadAll(r)
-	output := string(res)
-
-	os.Stdout = stdOut
-
-	return output == expected
+	assertStdOut(t, expected, exec, "Argument validation for arguments with validator functions is buggy")
 }
 
 func BenchmarkArgsBuilder(b *testing.B) {
